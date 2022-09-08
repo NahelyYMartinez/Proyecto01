11d6aa1c48cd68b33dcd675be23b2f38f6291e0d
NahelyYMartinez <113081527+NahelyYMartinez@users.noreply.github.com>
2022-09-07 23:45:44 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,392 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+  <head>
+    <base href="" />
+	<!-- Basic Page Needs
+
+     ================================================== -->
+	 
+	 <meta charset="utf-8">
+	 
+	 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    
+	 <link rel="icon" type="" href="images/logoM.png">
+	
+     <title>Trapiche</title>
+    
+     <meta name="description" content="">
+    
+     <meta name="keywords" content="">
+    
+     <meta name="author" content="">
+
+	 
+	 <!-- Mobile Specific Metas
+    
+     ================================================== -->
+    
+     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    
+     <meta name="format-detection" content="telephone=no">
+
+	 
+	 <!-- Web Font
+	 ============================================= -->
+	 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet">
+	
+	
+	<!-- CSS
+    
+     ================================================== -->
+	 
+	 
+	<!-- Theme Color
+	============================================= -->
+	<link rel="stylesheet" id="color" href="css/red.css">
+    
+	
+	<!--  Style
+	============================================= -->
+    <link rel="stylesheet" href="css/stilo1.css">
+
+	
+	<!-- This page
+	============================================= -->
+    <link href="css/stilo_revolucionario.css" rel="stylesheet">
+	<link href="css/settings.css" rel="stylesheet">
+	
+	
+	<!-- Bootstrap
+	============================================= -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    
+	
+
+	
+	
+	<!-- Header Scripts
+    
+    ================================================== -->
+	
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	
+	
+
+	</head>
+    <body class="fixed-header">
+	
+		
+	<?php include("voz.php")?>
+		<!-- Document Wrapper
+		============================================= -->
+		<div id="wrapper" class="clearfix">
+		
+		
+			<!-- Top Toggle
+			============================================= -->
+			<div class="top-sec">
+				<div class="top-sec-detail" id="top-sec-detail">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-6">
+								<ul class="contact-info">
+									<li><i class="fa fa-mobile"></i> (+591) 78180905</li>
+									<li><i class="fa fa-envelope"></i> <a href="#.">info@1rodemayo.com</a></li>
+								</ul>
+							</div>
+							<div class="col-md-6">
+								<ul class="social2">
+									<li><a href="#."><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#."><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#."><i class="fa fa-google-plus"></i></a></li>
+									<li><a href="#."><i class="fa fa-skype"></i></a></li>
+									<li><a href="#."><i class="fa fa-youtube"></i></a></li>
+									<li><a href="#."><i class="fa fa-dribbble"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+				<a href="#." class="toggle-btn" id="toggle-btn"><i class="fa fa-plus"></i></a>
+			</div>
+		
+		
+		
+		<!-- Main banner
+		============================================= -->
+		<section class="slider-revolution clearfix" data-stellar-background-ratio="0.3">
+    	
+			<div class="slider-revolution-overlay"></div>
+             
+			 	
+				<!-- Header
+				============================================= -->
+				<header class="medicom-header">
+                
+				
+					<div class="container">
+					
+						<!-- Primary Navigation
+						============================================= -->
+						<nav class="navbar navbar-default" role="navigation">
+						
+							<!-- Brand and toggle get grouped for better mobile display
+							============================================= -->
+							
+							<div class="navbar-header">
+								
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#primary-nav">
+								  <span class="sr-only">Toggle navigation</span>
+								  <span class="icon-bar"></span>
+								  <span class="icon-bar"></span>
+								  <span class="icon-bar"></span>
+								</button>
+								
+								<a class="navbar-brand" href="index.html"><img src="images/logoN.png" alt="" title=""></a>
+							
+							</div>
+						
+							
+							<div class="collapse navbar-collapse navbar-right" id="primary-nav">
+								
+								<ul class="nav navbar-nav">
+									
+									
+								<li class="dropdown active">
+									<a href="index.php" class="dropdown-toggle" data-toggle="dropdown">Inicio</a>
+									
+								</li>					
+										
+										<li><a href="nosotros.php">Sobre Nosotros</a></li>
+	
+									
+									<li><a href="contactos.php">Contactos</a></li>
+
+									<li><a href="AsisB/asistente.html">Asistente</a></li>
+									
+									<li><a href="mcventas/index.php">Iniciar o registrarse</a></li>
+									
+								
+			
+								  
+								</ul>
+								
+							</div>
+							
+						</nav>
+						
+					</div>
+						
+					<div class="header-bottom-line"></div>
+					
+				</header>
+			
+			
+			<div id="content-index" class="no-margin-top">
+			
+			
+				<!-- Banner
+				============================================= -->
+				<div class="container">
+		   
+					<div class="tp-banner-container">
+						
+						<div class="tp-banner">
+							
+							<ul>	
+								
+								
+								<!-- Carrucel
+								============================================= -->
+								<li data-transition="slideright" data-slotamount="6" data-thumb=""> 
+									<img src="images/blank.png" alt="" />	
+									<div class="caption lfr" data-x="650" data-y="10" data-speed="2000" data-start="900" data-easing="easeOutExpo">
+									<img src="images/doctoresA.png" alt=""/></div>	
+									<div class="caption sft big_white" data-x="0" data-y="130" data-speed="3000" data-start="1700" data-easing="easeOutExpo">Tendencias Cultural  </div>
+									<div class="caption sfr medium_grey" data-x="0" data-y="220" data-speed="1000" data-start="2500" data-easing="easeOutExpo">Santa Cruz Bolivia<br />
+									<br />
+					</div>
+										
+								</li>
+								<li data-transition="slideright" data-slotamount="6" data-thumb=""> 
+									<img src="images/blank.png" alt="" />	
+									<div class="caption lfr" data-x="650" data-y="10" data-speed="2000" data-start="900" data-easing="easeOutExpo">
+									<img src="images/doctoresA.png" alt=""/></div>	
+									<div class="caption sft big_white" data-x="0" data-y="130" data-speed="3000" data-start="1700" data-easing="easeOutExpo">Tendencias Cultural</div>
+									<div class="caption sfr medium_grey" data-x="0" data-y="220" data-speed="1000" data-start="2500" data-easing="easeOutExpo">Santa Cruz Bolivia<br />
+									<br />
+					</div>
+										
+								</li>
+								
+								
+							
+							</ul>
+																		
+						</div>	
+						
+					</div>
+	   
+				</div>
+                
+		</section>
+
+		
+		
+		<!--Seccion artesana;
+		============================================= -->
+		<section class="creative-sec">
+			
+			<div class="container">
+				
+				<div class="text-center">
+					<h1 class="light"> La mejor tendencia originaria de Bolivia</h1>
+					<p class="lead"></p>
+				</div>
+				
+			   <div class="creative-sec-thumb one">
+					<figure>
+						<span class="overlay"><a href=""><i class="fa fa-eye"></i></a></span>
+						<img src="images/sobre_N.jpg" width="154" class="img-circle img-thumbnail img-responsive" alt="" title="">
+						<figcaption>Llaveros</figcaption>
+					</figure>
+				</div>
+				
+				<div class="creative-sec-thumb two">
+					<figure>
+						<span class="overlay"><a href=""><i class="fa fa-eye"></i></a></span>
+						<img src="images/103.jfif" width="266" class="img-circle img-thumbnail img-responsive" alt="" title="">
+						<figcaption>Tronquitos</figcaption>
+					</figure>
+				</div>
+				
+				<div class="creative-sec-thumb three">
+					<figure>
+						<span class="overlay"><a href=""><i class="fa fa-eye"></i></a></span>
+						<img src="images/105.jfif" width="162" class="img-circle img-thumbnail img-responsive" alt="" title="">
+						<figcaption>Portallaves</figcaption>
+					</figure>
+				</div>
+				
+				<div class="creative-sec-thumb four">
+					<figure>
+						<span class="overlay"><a href=""><i class="fa fa-eye"></i></a></span>
+						<img src="images/10.jfif" width="305" class="img-circle img-thumbnail img-responsive" alt="" title="">
+						<figcaption>Artesania-madera</figcaption>
+					</figure>
+				</div>
+				
+			</div>
+			
+		</section>
+    
+		
+		
+	
+    
+		<div class="height30"></div>
+    
+	
+
+		
+	
+		<div class="colourfull-row"></div>
+    
+	
+                    
+	<!-- Redes Sociales
+	============================================= -->
+
+		<center><ul class="social3 clearfix">
+			<li><a href="#."><i class="fa fa-facebook"></i></a></li>
+			<li><a href="#."><i class="fa fa-twitter"></i></a></li>
+			<li><a href="#."><i class="fa fa-google-plus"></i></a></li>
+			<li><a href="#."><i class="fa fa-skype"></i></a></li>
+			<li><a href="#."><i class="fa fa-youtube"></i></a></li>
+			<li><a href="#."><i class="fa fa-dribbble"></i></a></li>
+		</ul></center>
+	
+
+	
+	<!-- All Javascript 
+	============================================= -->
+	<script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.stellar.js"></script>
+	<script src="js/jquery-ui-1.10.3.custom.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/counter.js"></script>
+    <script src="js/waypoints.js"></script>
+	<script src="js/jquery.uniform.js"></script>
+    <script src="js/easyResponsiveTabs.js"></script>
+	<script src="js/jquery.fancybox.pack.js"></script>
+	<script src="js/jquery.fancybox-media.js"></script>
+	<script src="js/jquery.mixitup.js"></script>
+	<script src="js/forms-validation.js"></script>
+	<script src="js/jquery.jcarousel.min.js"></script>
+	<script src="js/jquery.easypiechart.min.js"></script>
+	<script src="js/scripts.js"></script>
+    
+	<!-- This page
+	============================================= -->
+	<script src="js/jquery.themepunch.plugins.min.js"></script>			
+    <script src="js/jquery.themepunch.revolution.min.js"></script>
+
+	
+    <script>
+		
+		(function () {
+			
+			// Revolution slider
+			var revapi;
+			revapi = jQuery('.tp-banner').revolution(
+			{
+				delay:9000,
+				startwidth:1170,
+				startheight:576,
+				hideThumbs:200,
+				fullWidth:"on",
+				forceFullWidth:"on"
+			});
+			
+			
+			/* ------------------------------------------------------------------------ 
+			   SMALL HEADER 
+			------------------------------------------------------------------------ */ 
+			function checkWidth() {
+				var windowSize = $(window).width();
+
+				if (windowSize >= 767) {
+					jQuery(window).scroll(function() {    
+						var scroll = jQuery(window).scrollTop();	
+						if (scroll >= 1) {
+							jQuery(".fixed-header").addClass("small-header");
+							jQuery(".navbar-brand img" ).attr("src", "images/logoY.png");
+						}
+						else {
+							jQuery(".fixed-header").removeClass("small-header");
+							jQuery(".navbar-brand img" ).attr("src", "images/logoN.png");
+						}
+					});
+				}
+			}
+
+			// Execute on load
+			checkWidth();
+			// Bind event listener
+			$(window).resize(checkWidth);
+			
+			
+			
+			
+
+			
+			
+		
+		})(jQuery);
+
+	</script>
+    <!-- Código de instalación Cliengo para silvaleon7018895@gmail.com --> <script type="text/javascript">(function () { var ldk = document.createElement('script'); ldk.type = 'text/javascript'; ldk.async = true; ldk.src = 'https://s.cliengo.com/weboptimizer/62b4793fbcacf7002ae85a48/62b47941bcacf7002ae85a4c.js?platform=view_installation_code'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ldk, s); })();</script>
+  </body>
+</html>
